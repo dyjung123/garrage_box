e8e76149651b71e4c386c7d9715cf28f48bb0866
dyjung123 <jeongdongryun@gmail.com>
2021-09-20 16:37:04 +0900

    Replace path() to url() and Fix typing error

--- app/table/serializer.py
+++ app/table/serializer.py
@@ -6,7 +6,7 @@ from .models import User, Plan, TimeTable
 class UserSerializer(serializers.ModelSerializer):
   class Meta:
     model = User
-    fields = ('id', 'uesrname', 'email', 'enabled', 'created_at', 'updated_at')
+    fields = ('id', 'username', 'email', 'enabled', 'created_at', 'updated_at')
 
 
 class PlanSerializer(serializers.ModelSerializer):
--- app/time_table/urls.py
+++ app/time_table/urls.py
@@ -14,17 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.conf import settings
+from django.conf.urls import url, include
 from django.conf.urls.static import static
 
 from upload.views import image_upload
 from table.urls import router as table_router
 
 urlpatterns = [
-    path("", image_upload, name="upload"),
-    path('admin/', admin.site.urls),
-    path(r'^api/', include(table_router.urls)),
+    url(r'^$', image_upload, name="upload"),
+    url(r'^admin/', admin.site.urls),
+    url(r'^api/', include(table_router.urls)),
 ]
 
 if bool(settings.DEBUG):
