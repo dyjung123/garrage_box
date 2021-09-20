52f8203ca0b09891e87df671d5c043987054743e
dyjung123 <jeongdongryun@gmail.com>
2021-09-20 20:20:05 +0900

    Install django-cors-headers and Add cors whitelist in settings.py

--- app/time_table/settings.py
+++ app/time_table/settings.py
@@ -43,9 +43,11 @@ INSTALLED_APPS = [
     "upload",
     "table",
     "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,6 +56,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = False
+
+CORS_ORIGIN_WHITELIST = (
+    'https://garage-box.netlify.app',
+)
 
 ROOT_URLCONF = 'time_table.urls'
 
