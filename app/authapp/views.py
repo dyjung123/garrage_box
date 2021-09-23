09c1db58ca433f4b7e623f8c9880ad85e6bed068
dyjung123 <jeongdongryun@gmail.com>
2021-09-23 18:01:30 +0900

    Register logout URL with logout function with @login_required

--- app/authapp/views.py
+++ app/authapp/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout as django_logout
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -7,3 +10,12 @@ def index(request):
 
 def reports(request):
     return render(request, 'authapp/reports.html')
+
+
+@login_required
+def logout(request):
+    django_logout(request)
+    domain = settings.SOCIAL_AUTH_AUTH0_DOMAIN
+    client_id = settings.SOCIAL_AUTH_AUTH0_KEY
+    return_to = 'http://127.0.0.1:8000'
+    return redirect(f'https://{domain}/v2/logout?client_id={client_id}&returnTo={return_to}')
