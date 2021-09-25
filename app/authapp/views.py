882de02c500f58bfb15299fcdabf61bc91a85a32
dyjung123 <jeongdongryun@gmail.com>
2021-09-25 17:01:40 +0900

    Change logout url

--- app/authapp/views.py
+++ app/authapp/views.py
@@ -34,7 +34,7 @@ def logout(request):
     django_logout(request)
     domain = settings.SOCIAL_AUTH_AUTH0_DOMAIN
     client_id = settings.SOCIAL_AUTH_AUTH0_KEY
-    return_to = 'http://localhost:8000/authapp'
+    return_to = 'https://dongryun.pythonanywhere.com/authapp'
     return redirect(f'https://{domain}/v2/logout?client_id={client_id}&returnTo={return_to}')
 
 
