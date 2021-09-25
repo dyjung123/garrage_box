458cdcbca94a4bfb65b2841902b9ac5b92f3fa9e
dyjung123 <jeongdongryun@gmail.com>
2021-09-26 01:08:02 +0900

    Allow any get users

--- app/auth0authorization/views.py
+++ app/auth0authorization/views.py
@@ -59,6 +59,7 @@ def private_scoped(request):
 
 
 class UserAPI(APIView):
+    @permission_classes([AllowAny])
     def get(self, request):
         queryset = User.objects.all()
         print(queryset)
